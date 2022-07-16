c682daf8529b5af169de946cbc7ace3510362868
Jlexand <zlexandz@gmail.com>
2022-07-16 10:38:53 +0300

    #7 1/2 урока, разбор react-loading и компонента sceleton, установка react-router

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react'
 import Header from './Components/Header'
 import Categories from './Components/Categories'
 import Sort from './Components/Sort'
+import PizzaSkeleton from './Components/PizzaBlock/Skeleton'
 import PizzaBlock from './Components/PizzaBlock'
 
 import './scss/app.scss'
@@ -10,11 +11,15 @@ import './scss/app.scss'
 function App() {
 
   const [items, setItems] = useState([])
+  const [isLoading, setIsLoading] = useState(true);
 
   React.useEffect(() => {
     fetch('https://62d057061cc14f8c0888fda2.mockapi.io/items')
       .then((res) => res.json())
-      .then((json) => setItems(json))
+      .then((json) => {
+        setItems(json);
+        setIsLoading(false)
+      })
   }, [])
 
   return (
@@ -28,9 +33,11 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {items.map((obj) => (
-              <PizzaBlock key={obj.id} {...obj} />
-            ))}
+            {
+              isLoading ?
+                [...Array(6)].map((_, index) => <PizzaSkeleton key={index} />)
+                : items.map((obj) => (<PizzaBlock key={obj.id} {...obj} />))
+            }
           </div>
         </div>
       </div>
---
+++ src/Components/PizzaBlock/Skeleton.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ContentLoader from 'react-content-loader';
+
+const PizzaSkeleton = (props) => (
+  <ContentLoader
+    className="pizza-block"
+    speed={2}
+    width={280}
+    height={465}
+    viewBox="0 0 280 465"
+    backgroundColor="#f3f3f3"
+    foregroundColor="#ecebeb"
+    {...props}>
+    <circle cx="139" cy="139" r="120" />
+    <rect x="30" y="281" rx="10" ry="10" width="220" height="24" />
+    <rect x="35" y="332" rx="10" ry="10" width="212" height="78" />
+    <rect x="40" y="427" rx="10" ry="10" width="91" height="33" />
+    <rect x="77" y="443" rx="0" ry="0" width="15" height="0" />
+    <rect x="147" y="427" rx="10" ry="10" width="96" height="35" />
+    <rect x="188" y="448" rx="0" ry="0" width="1" height="3" />
+  </ContentLoader>
+);
+
+export default PizzaSkeleton;
