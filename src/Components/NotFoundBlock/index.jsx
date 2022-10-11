9c2e1f56ea8d3ed7ef820b251b2dcd226002b13b
Jlexand <zlexandz@gmail.com>
2022-10-11 21:29:07 +0300

    Conflicts resolved

--- src/Components/NotFoundBlock/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import style from './style.module.scss';
-
-function NotFoundBlock() {
-  return <div className={style.notfound}>Ничего не найдено((</div>;
-}
-
-export default NotFoundBlock;
