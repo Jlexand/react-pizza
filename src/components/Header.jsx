d0054644e623ff974adba79539574577252cf1db
Jlexand <zlexandz@gmail.com>
2022-10-30 15:47:08 +0300

    7 lesson

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
+import { Link } from 'react-router-dom';
+
 import logoSvg from '../assets/img/pizza-logo.svg';
 
 function Header() {
   return (
     <div className="header">
       <div className="container">
+        <Link to="/">
         <div className="header__logo">
           <img width="38" src={logoSvg} alt="Pizza logo" />
           <div>
@@ -11,8 +14,10 @@ function Header() {
             <p>самая вкусная пицца во вселенной</p>
           </div>
         </div>
+        </Link>
+        <Link to="/cart">
         <div className="header__cart">
-          <a href="/cart.html" className="button button--cart">
+          <span className="button button--cart">
             <span>520 ₽</span>
             <div className="button__delimiter"></div>
             <svg
@@ -44,8 +49,9 @@ function Header() {
               />
             </svg>
             <span>3</span>
-          </a>
+          </span>
         </div>
+        </Link>
       </div>
     </div>
   );
---
+++ src/pages/Home.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+import Categories from '../components/Categories';
+import Sort from '../components/Sort';
+import PizzaBlock from '../components/PizzaBlock/PizzaBlock';
+import Sceleton from '../components/PizzaBlock/sceleton';
+
+const Home = () => {
+
+    const [items, setItems] = React.useState([]);
+    const [isLoading, setIsLoading] = React.useState(true);
+  
+    React.useEffect(() => {
+      fetch('https://62d057061cc14f8c0888fda2.mockapi.io/items')
+        .then((res) => res.json())
+        .then((json) => {
+          setItems(json);
+          setIsLoading(false);
+        });
+    }, [])
+
+  return (
+    <>
+        <div className="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            {
+              isLoading
+                ? [...new Array(6)].map((_, index)=> <Sceleton key={index}/>)
+                : items.map((obj, i) => (<PizzaBlock {...obj} key={i} />))
+            }
+          </div>
+    </>
+  )
+}
+
+export default Home
