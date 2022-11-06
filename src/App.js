29799cbed3843970b5b96d672e2e5b46eb43e437
Jlexand <zlexandz@gmail.com>
2022-11-06 18:30:29 +0300

    12 lesson

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
+import { useSelector, useDispatch } from 'react-redux';
+import { decrement, increment } from './redux/slices/filterSlice';
 
 import Header from './components/Header';
 import Cart from './pages/Cart';
@@ -14,6 +16,8 @@ import './scss/app.scss';
 export const SearchContext = React.createContext();
 
 function App() {
+  const count = useSelector((state) => state.filter.count)
+  const dispatch = useDispatch()
 
   const [searchValue, setSearchValue] = React.useState('');
 
@@ -23,11 +27,26 @@ function App() {
         <Header />
         <div className="content">
           <div className="container">
-            <Routes>
+            <div>
+              <button
+                aria-label="Increment value"
+                onClick={() => dispatch(increment())}
+              >
+                Increment
+              </button>
+              <span>{count}</span>
+              <button
+                aria-label="Decrement value"
+                onClick={() => dispatch(decrement())}
+              >
+                Decrement
+              </button>
+            </div>
+            {/* <Routes>
               <Route path='/' element={<Home />} />
               <Route path='/cart' element={<Cart />} />
               <Route path='*' element={<NotFound />} />
-            </Routes>
+            </Routes> */}
           </div>
         </div>
       </SearchContext.Provider>
